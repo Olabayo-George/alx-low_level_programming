b935e85afb7492eca36eb7c976846a4efabed3a6
Olabayo-George <olabayooguns@gmail.com>
2023-03-19 01:38:36 -0700

    assembly

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,12 @@
  */
 int main(void)
 {
+	int size_of_char = sizeof(char);
+       	int size_of_int = sizeof(int);
+       	int size_of_long = sizeof(long);
+	int size_of_long_int = sizeof(long int);
+	int size_of_long_long_int = sizeof(long long int);
+	int size_of_float = sizeof(float);
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of a int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
